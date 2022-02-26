1934a5bdbc22befa14d532ffe7b6e36cf6257ec3
J-FIlip <filip.jugkala@yahoo.com>
2022-02-26 02:35:41 +0100

    vue testing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<section class="hero">
+  <div class="hero-body">
+    <p class="title">
+      Vue test 2
+    </p>
+    <p class="subtitle">
+      Bulma SASS enabled
+    </p>
+  </div>
+</section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+
+// importing method from vue
 import { createApp } from 'vue'
+// importing component 
 import App from './App.vue'
 
+// mounting main app component to div with id = app
 createApp(App).mount('#app')
+
+require ("./sass/main.scss")
